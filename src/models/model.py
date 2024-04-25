7e753e5b6b55e507e46a05c5b4b527b50c915481
NiktiaGorbunov <grb.nikita@yandex.ru>
2024-04-25 22:43:37 +0500

    test

--- src/models/model.py
+++ src/models/model.py
@@ -1,3 +1,3 @@
-print(           "Hello world! " )
+print(                "Hello world! ")
 Ddasdasd="dassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
 print(Ddasdasd)
