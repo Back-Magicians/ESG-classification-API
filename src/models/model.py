cca29340406c6e689d97abb9b63e0a9222f1ca8f
Александр Соломеин <alex@callibri.ru>
2024-05-19 22:05:35 +0500

    used "black ."

--- src/models/model.py
+++ src/models/model.py
@@ -12,12 +12,10 @@ from data.example import ARTICLE_TEXT
 
 
 def mean_pooling(model_output, attention_mask):
-    token_embeddings = (
-        model_output
-    )
+    token_embeddings = model_output
     input_mask_expanded = attention_mask.unsqueeze(-1)
     input_mask_expanded = input_mask_expanded.expand(token_embeddings.size()).float()
-    
+
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
         input_mask_expanded.sum(1), min=1e-9
     )
@@ -57,7 +55,7 @@ class ESGify(MPNetPreTrainedModel):
 
         # apply sigmoid
         logits = 1.0 / (1.0 + torch.exp(-logits))
-        
+
         return logits
 
 
