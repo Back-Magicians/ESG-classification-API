4d2249c0b9698b8347e8c99d7ce23ca4e07a9742
NiktiaGorbunov <grb.nikita@yandex.ru>
2024-05-29 15:58:45 +0500

    refactor code style

--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app, VIEWS_PATH
+from main import app
 import sys
 import os
 
@@ -7,8 +7,10 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 client = TestClient(app)
 
+
 def test_root_endpoint():
-    """Тестирование корневой конечной точки (/) для проверки корректного возврата HTML содержимого."""
+    """Тестирование корневой конечной точки (/)
+    для проверки корректного возврата HTML содержимого."""
     response = client.get("/")
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/html; charset=utf-8"
@@ -16,13 +18,16 @@ def test_root_endpoint():
 
 
 def test_predict_endpoint():
-    """Тестирование конечной точки предсказаний (/predict/) для проверки корректной обработки текста."""
+    """Тестирование конечной точки предсказаний (/predict/)
+    для проверки корректной обработки текста."""
     sample_text = "Это пример текста для классификации ESG рисков."
     response = client.post("/predict/", json={"text": sample_text})
     assert response.status_code == 200
-    assert  len(response.json()) > 0
+    assert len(response.json()) > 0
+
 
 def test_predict_endpoint_validation():
-    """Тестирование конечной точки предсказаний (/predict/) для проверки корректной обработки некорректного ввода."""
+    """Тестирование конечной точки предсказаний (/predict/)
+    для проверки корректной обработки некорректного ввода."""
     response = client.post("/predict/", json={})
     assert response.status_code == 422  # Статус код для ошибок валидации
